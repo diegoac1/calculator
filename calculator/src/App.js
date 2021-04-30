d49d1744057b448aade7116d0ab8057788788b32
diegoac <diegoacosta005@gmail.com>
2021-04-30 14:32:41 -0300

    Teste

---
+++ calculator/src/App.js
@@ -0,0 +1,14 @@
+import Panel from './Panel.js';
+
+function dis(val)
+{
+    document.getElementById("result").value+=val
+}
+
+function App() {
+  return (
+    <Panel />
+  );
+}
+
+export default App;
